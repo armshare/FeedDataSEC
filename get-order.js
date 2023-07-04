2a25edf86d25f5a64fad59ee40d9b14cbfdb59c8
arm <armshare.it@gmail.com>
2023-07-04 22:59:27 +0700

    update module

--- get-order.js
+++ get-order.js
@@ -1,20 +1,13 @@
 
 const sql = require('mssql');
 const moment = require('moment')
-const dbConfig = require('./keys/db-config');
+//const dbConfig = require('./keys/db-config');
 
 const getOrders = async () => {
   try {
     let pool = await sql.connect(dbConfig);
      let funds = await pool.request().query(`SELECT * FROM  dim_funds_settrade order by symbol asc`);
-   // let funds = await pool.request().query(`select * from risk_null_temp`);
-    
-    // console.log('funds ', funds)
-    // const fund = funds.recordset.map(i => ({
-    //   "proj_abbr_name": i.proj_abbr_name,
-    //   "proj_id": i.proj_id //,
-    // //  "inception_date":  moment(i.regis_date.split(' ')[0].split("/").reverse().join("-")).format("YYYY-MM-DD") 
-    // }));
+ 
     const fund = funds.recordset.map(i => ( i.symbol));
     return fund;
   }
